0e42f2c0512b5600923b850d28cd196e8cac4d01
TechyChristofff <chris.smith@thetecheffect.co.uk>
2016-05-06 22:37:02 +0100

    Git revert

--- FluidSimulatorWrapper/FluidUnityPlugin/SPH_Controller.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#if UNITY_EDITOR || UNITY_STANDALONE
-#define EXPORT_API __declspec(dllexport) __stdcall
-#elif UNITY_WIN
-#define EXPORT_API __declspec(dllexport)
-#else
-#define EXPORT_API
-#endif
